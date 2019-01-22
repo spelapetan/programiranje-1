7ac55e7f212709a8164344736ffae8472cd714fd
Špela Petan <spela.petan@student.fmf.uni-lj.si>
2019-01-22 17:48:28 +0100

    22.1.

--- izpiti/2017-12-07/poskusni-izpit-2017-12-07.ml
+++ izpiti/2017-12-07/poskusni-izpit-2017-12-07.ml
@@ -4,23 +4,32 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame dve celi števili ter vrne njuno vsoto.
    Primer: /sestej 2 3 = 5/ *)
-let sestej = failwith "dopolni me"
+
+let sestej x y = x + y
 
 (* 1.2) Definirajte funkcijo, ki svojemu argumentu prišteje 3.
    Primer: /pristej_tri 10 = 13/ *)
-let pristej_tri = failwith "dopolni me"
+
+let pristej_tri x = x + 3
 
 (* 1.3) Definirajte funkcijo, ki vsem elementom seznama prišteje 5.
    Primer: /vsem_pristej_pet [1; 2] = [6; 7]/ *)
-let vsem_pristej_pet = failwith "dopolni me"
+
+let rec vsem_pristej_pet = function
+  | [] -> []
+  | x :: xs -> (x + 5) :: vsem_pristej_pet xs
 
 (* 1.4) Definirajte funkcijo, ki vrne zadnjo komponento nabora s tremi elementi.
    Primer: /tretji (1, "horse", [None]) = [None]/ *)
-let tretji = failwith "dopolni me"
+
+let tretji nabor =
+  match nabor with
+  | (_, _, x) -> x
 
 (* 1.5) Definirajte funkcijo, ki vzame dve funkciji ter vrne njun kompozitum.
    Primer: /kompozitum succ string_of_int 5 = "6"/ *)
-let kompozitum = failwith "dopolni me"
+
+let kompozitum f g x = g (f x) 
 
 
 (* ======================================= *)
@@ -32,18 +41,42 @@ let kompozitum = failwith "dopolni me"
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
+
+type 'a drevo = 
+  | Empty
+  | Node of 'a * 'a drevo
 
 (* 2.2) Napišite funkcijo, ki vrne koren danega rožnega drevesa. *)
-let koren = failwith "dopolni me"
+
+let koren = function
+  | Empty -> None
+  | Node(x, tree) -> x
 
 (* 2.3) Napišite funkcijo, ki preveri, ali drevo celih števil vsebuje kakšno negativno število. *)
-let kaksno_negativno = failwith "dopolni me"
+
+let rec kaksno_negativno = function
+  | Empty -> false
+  | Node (x, tree) ->
+      if x < 0 then 
+        true
+      else
+        kaksno_negativno tree
 
 (* 2.4) Sestavite funkcijo, ki sprejme naravno število ter sestavi (poljubno)
    drevo, ki ima toliko otrok.
    Namig: napišite pomožno funkcijo, ki ustvari poljuben seznam dane dolžine. *)
-let drevo_z_veliko_otroci = failwith "dopolni me"
+
+let rec ustvari_seznam l max =
+  if l <= 0 then
+    []
+  else
+    Random.int max :: (ustvari_seznam (l-1) max)
+
+
+let drevo_z_veliko_otroci n = 
+  let koren = Random.int n in
+  let sez = ustvari_seznam n (n * 10) in
+  Node (koren, sez)
 
 (* 2.5) Sestavite funkcijo, ki izračuna število vseh vozlišč v drevesu.
    Če želite vse točke, mora biti funkcija repno rekurzivna.
@@ -51,4 +84,5 @@ let drevo_z_veliko_otroci = failwith "dopolni me"
    Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
    na primer List.map, niso repno rekurzivne, zato se jim raje
    izognite. *)
+
 let velikost = failwith "dopolni me"
--- izpiti/2017-12-18/bonus-izpit-2017-12-18.ml
+++ izpiti/2017-12-18/bonus-izpit-2017-12-18.ml
@@ -4,27 +4,36 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame tri cela števila ter vrne njihov produkt.
    Primer: /zmnozi 2 3 4 = 24/ *)
- let zmnozi = failwith "dopolni me"
+ let zmnozi x y z = x * y * z
 
 (* 1.2) Definirajte funkcijo, ki vzame celo število x in celo število k, ter
    vrne vrednost izraza x^3 + k.
    Primer: /afin_kub 2 1 = 9/ *)
- let afin_kub = failwith "dopolni me"
+ let afin_kub x k = x * x * x + k
 
 (* 1.3) Definirajte funkcijo, ki vzame seznam in izračuna seznam vrednosti funkcije
    f(x) = x^3 + 2 za elemente vhodnega seznama.
    Primer: /vse_kubiraj_in_pristej_dva [1; 2; 3] = [3; 10; 29]/ *)
- let vse_kubiraj_in_pristej_dva = failwith "dopolni me"
+ let rec vse_kubiraj_in_pristej_dva f = function
+  | [] -> []
+  | x :: xs -> f x :: (vse_kubiraj_in_pristej_dva f xs)
 
 (* 1.4) Definirajte funkcijo, ki varno vrne zadnji element seznama v primeru,
    da seznam ni prazen. Uporabite tip option.
    Primer: /zadnji_element [1; 2; 3] = Some 3/ *)
- let zadnji_element = failwith "dopolni me"
+ let rec zadnji_element = function
+  | [] -> None
+  | x :: [] -> Some(x)
+  | x :: xs -> zadnji_element xs
 
 (* 1.5) Definirajte funkcijo, ki izračuna n-to Fibonaccijevo število.
    Pri tem upoštevamo začetna pogoja /fibonacci 0 = 1/ in /fibonacci 1 = 1/.
    Primer: /fibonacci 20 = 10946/ *)
- let fibonacci = failwith "dopolni me"
+ let rec fibonacci n = 
+  match n with
+  | 0 -> 1
+  | 1 -> 1
+  | _ -> fibonacci (n - 1) + fibonacci (n - 2)
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
@@ -35,7 +44,8 @@
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
+
+type 'a drevo = Node of 'a * 'a drevo list
 
 (* 2.2) Definirajte naslednja rožna drevesa:
 
@@ -46,16 +56,25 @@ type 'a drevo = DopolniMe
 
  *)
 
-let t = failwith "dopolni me"
-let t' = failwith "dopolni me"
-let t'' = failwith "dopolni me"
+let t = Node(1, [])
+let t' = Node(2, [t; t])
+let t'' = Node(3, [Node(-1, []); t'; Node(0, [])])
 
 (* 2.3) Definirajte funkcijo, ki preveri ali je dano rožno drevo list drevesa,
    torej ima prazen gozd poddreves. *)
-let je_list = failwith "dopolni me"
+let je_list list drevo =
+  match drevo with
+  | (_, []) -> false
+  | (_, xs) ->
+      List.mem list xs
+  
 
 (* 2.4) Definirajte funkcijo, ki preveri, ali drevo celih števil vsebuje zgolj pozitivna števila. *)
-let vsa_pozitivna = failwith "dopolni me"
+let vsa_pozitivna tree =
+  match tree with
+  | (x, []) -> x > 0
+  | (x, y :: ys) ->
+    
 
 (* 2.5) Definirajte funkcijo, ki izračuna največjo širino rožnega drevesa, torej največjo dolžino
    gozda, ki se pojavi v kateremkoli vozlišču rožnega drevesa. *)
